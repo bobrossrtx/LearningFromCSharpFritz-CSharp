636323a3caf451106af2ac1671bc9ab26efec6dc
Owen Boreham <bobrossrtx@gmail.com>
2021-07-03 15:50:31 +0100

    Started learning classes

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Classes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      #region "Old Student object creation"
+      //var s = new Student();
+      //s.FirstName = "Owen";
+      //s.LastName = "Boreham";
+      //s.Age = 15;
+
+      //var codex = new Student()
+      //{
+      //  FirstName = "nfg",
+      //  LastName = "Codex",
+      //  Age = 20,
+      //};
+
+      //if (codex.Age > 18)
+      //{
+      //  codex.IsEnrolled = false;
+      //}
+      #endregion
+
+      // Create new students
+      var owen = new Student("Owen", "boreham", 14);
+      var bob = new Student("Bob", "Ross", 17);
+
+      // Add all the students to a list
+      Student[] students = { owen, bob };
+
+      foreach(Student student in students)
+      {
+        Console.WriteLine($"Name: {student.FirstName} {student.LastName}, \r\nAge: {student.Age}, \r\nEnrolled: {student.IsEnrolled}\r\n");
+      }
+      
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Classes/Classes/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+  internal class Student
+  {
+    public Student(string firstName, string lastName, int age)
+    {
+      this.FirstName = firstName;
+      this.LastName = lastName;
+      this.Age = age;
+
+      if (this.Age <= 18)
+      {
+        // Every student is enrolled by default if age 18 or less
+        this.IsEnrolled = true;
+      } else
+      {
+        this.IsEnrolled = false;
+      }
+    }
+
+    // Publicly accessable properties that define the FirstName, LastName
+    // and age of our Student.
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public bool IsEnrolled { get; private set; }
+     
+  }
+}
