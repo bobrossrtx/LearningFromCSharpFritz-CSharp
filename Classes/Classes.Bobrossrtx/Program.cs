be8b09f216f4de94224b1fb9142099f715294240
Owen Boreham <bobrossrtx@gmail.com>
2021-07-06 20:16:21 +0100

    Created Classes.Bobrossrtx project

---
+++ Classes/Classes.Bobrossrtx/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/**
+  * This app uses fake IP Addresses, The Ip Addresses used were generated at complete random
+  */
+namespace Classes.Bobrossrtx
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Shop myShop = new Shop("Bobross's Shop");
+      myShop.Products = 30;
+
+      Shop otherShop = new Shop("Random Shop", "199.117.207.32" , 10);
+
+      Shop[] allShops = { myShop, otherShop };
+
+      foreach(Shop shop in allShops)
+      {
+        Console.WriteLine($"Shop: {shop.ShopName}\nIpAddress: {shop.Address}\nProducts: {shop.Products}\r\n");
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Classes/Classes.Bobrossrtx/Shop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Bobrossrtx
+{
+  public class Shop
+  {
+    private static Random _random = new Random();
+
+    private string GetRandomIpAddress()
+    {
+      return string.Format($"{_random.Next(0,255)}.{_random.Next(0, 255)}.{_random.Next(0, 255)}.{_random.Next(0, 255)}");
+    }
+
+    public Shop(string shopName)
+    {
+      this.ShopName = shopName;
+      this.Address = GetRandomIpAddress();
+    }
+
+    public Shop(string shopName, int products)
+    {
+      this.ShopName = shopName;
+      this.Address = GetRandomIpAddress();
+      this.Products = products;
+    }
+
+    public Shop(string shopName, string address, int products)
+    {
+      this.ShopName = shopName;
+      this.Address = address;
+      this.Products = products;
+
+    }
+
+    public string ShopName { get; private set; }
+    public string Address { get; private set; }
+    public int Products { get; set; }
+  }
+}
